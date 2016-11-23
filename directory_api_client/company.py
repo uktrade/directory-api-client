e9e7a9296500d78a2f403f16ea42a44575d9c4c2
Rich Tier <rikatee@gmail.com>
2016-11-23 21:21:02 +0000

    Add case study endpoints

--- directory_api_client/company.py
+++ directory_api_client/company.py
@@ -6,7 +6,8 @@ class CompanyAPIClient(BaseAPIClient):
     endpoints = {
         'profile': '/user/{sso_id}/company/',
         'validate-company-number': '/validate/company-number/',
-        'companies-house-profile': '/company/companies-house-profile/',
+        'case-study-detail': '/company/case-study/{id}/',
+        'case-study-list': '/company/case-study/',
     }
 
     def update_profile(self, sso_user_id, data):
@@ -24,11 +25,19 @@ class CompanyAPIClient(BaseAPIClient):
         url = self.endpoints['profile'].format(sso_id=sso_user_id)
         return self.get(url)
 
-    def retrieve_companies_house_profile(self, number):
-        url = self.endpoints['companies-house-profile']
-        return self.get(url, {'number': number})
-
     def validate_company_number(self, number):
         url = self.endpoints['validate-company-number']
         params = {'number': number}
         return self.get(url, params=params)
+
+    def create_supplier_case_study(self, data):
+        url = self.endpoints['case-study-list']
+        return self.post(url, data=data)
+
+    def update_supplier_case_study(self, data, case_study_id):
+        url = self.endpoints['case-study-detail'].format(id=case_study_id)
+        return self.patch(url, data=data)
+
+    def delete_supplier_case_study(self, case_study_id):
+        url = self.endpoints['case-study-detail'].format(id=case_study_id)
+        return self.delete(url)
