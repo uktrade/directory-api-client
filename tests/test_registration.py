27b7abd3d1f776cab2b373044c4a2be66abfd8de
Rich Tier <rikatee@gmail.com>
2016-12-07 15:19:38 +0000

    Add company address verification hander and remove obsolete handlers

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -17,19 +17,3 @@ class EnrolmentAPIClientTest(TestCase):
         self.registration_client.send_form(
             form_data='form_data'
         )
-
-    @stub_request('https://example.com/enrolment/confirm/', 'post')
-    def test_confirm_email(self, stub):
-        self.registration_client.confirm_email(
-            confirmation_code='123'
-        )
-        request = stub.request_history[0]
-        assert request.json() == {'confirmation_code': '123'}
-
-    @stub_request('https://example.com/enrolment/verification-sms/', 'post')
-    def test_send_verification_sms(self, stub):
-        self.registration_client.send_verification_sms(
-            phone_number='333'
-        )
-        request = stub.request_history[0]
-        assert request.json() == {'phone_number': '333'}
--- tests/test_supplier.py
+++ tests/test_supplier.py
@@ -22,15 +22,3 @@ class SupplierAPIClientTest(TestCase):
     @stub_request('https://example.com/supplier/1/', 'get')
     def test_retrieve_profile(self, stub):
         self.client.retrieve_profile(sso_id=1)
-
-    @stub_request('https://example.com/validate/email-address/', 'get')
-    def test_validate_email_address(self, stub):
-        self.client.validate_email_address(email='jim@example.com')
-        request = stub.request_history[0]
-        assert request.query == 'company_email=jim%40example.com'
-
-    @stub_request('https://example.com/validate/phone-number/', 'get')
-    def test_validate_mobile_number(self, stub):
-        self.client.validate_mobile_number(mobile_number='07507605348')
-        request = stub.request_history[0]
-        assert request.query == 'mobile_number=07507605348'
