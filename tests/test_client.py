592d64fd2bf41bbb20d836ff838ae6d960731682
Richard Tier <rihard@highertier.com>
2017-04-07 16:58:11 +0100

    Add the shared signature auth library

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,25 +11,27 @@ class DirectoryAPIClientTest(TestCase):
 
     def setUp(self):
         self.base_url = 'https://example.com'
-        self.api_key = 'test'
-        self.client = DirectoryAPIClient(self.base_url, self.api_key)
+        self.key = 'test'
+        self.client = DirectoryAPIClient(self.base_url, self.key)
 
     def test_registration(self):
         assert isinstance(self.client.registration, EnrolmentAPIClient)
         assert self.client.registration.base_url == self.base_url
-        assert self.client.registration.api_key == self.api_key
+        assert (
+            self.client.registration.request_signer.signer.secret == self.key
+        )
 
     def test_company(self):
         assert isinstance(self.client.company, CompanyAPIClient)
         assert self.client.company.base_url == self.base_url
-        assert self.client.company.api_key == self.api_key
+        assert self.client.company.request_signer.signer.secret == self.key
 
     def test_supplier(self):
         assert isinstance(self.client.supplier, SupplierAPIClient)
         assert self.client.supplier.base_url == self.base_url
-        assert self.client.supplier.api_key == self.api_key
+        assert self.client.supplier.request_signer.signer.secret == self.key
 
     def test_buyer(self):
         assert isinstance(self.client.buyer, BuyerAPIClient)
         assert self.client.buyer.base_url == self.base_url
-        assert self.client.supplier.api_key == self.api_key
+        assert self.client.buyer.request_signer.signer.secret == self.key
