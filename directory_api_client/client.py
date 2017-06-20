c2dd212c8eea09c511eb8ea827d666a332aeef5f
Richard Tier <rihard@highertier.com>
2017-06-20 15:08:14 +0100

    Add handler for retrieving trusted source signup details

--- directory_api_client/client.py
+++ directory_api_client/client.py
@@ -1,7 +1,7 @@
 from directory_api_client.base import BaseAPIClient
 from directory_api_client.buyer import BuyerAPIClient
 from directory_api_client.company import CompanyAPIClient
-from directory_api_client.registration import EnrolmentAPIClient
+from directory_api_client.enrolment import EnrolmentAPIClient
 from directory_api_client.supplier import SupplierAPIClient
 from directory_api_client.notifications import NotificationsAPIClient
 from directory_api_client.exportopportunity import ExportOpportunityAPIClient
@@ -12,7 +12,7 @@ class DirectoryAPIClient(BaseAPIClient):
     def __init__(self, base_url=None, api_key=None):
         super(DirectoryAPIClient, self).__init__(base_url, api_key)
 
-        self.registration = EnrolmentAPIClient(base_url, api_key)
+        self.enrolment = EnrolmentAPIClient(base_url, api_key)
         self.company = CompanyAPIClient(base_url, api_key)
         self.supplier = SupplierAPIClient(base_url, api_key)
         self.buyer = BuyerAPIClient(base_url, api_key)
--- directory_api_client/enrolment.py
+++ directory_api_client/enrolment.py
@@ -5,6 +5,7 @@ class EnrolmentAPIClient(BaseAPIClient):
 
     endpoints = {
         'enrolment': '/enrolment/',
+        'trusted-code': '/trusted-code/{code}/'
     }
 
     def send_form(self, form_data):
@@ -12,3 +13,7 @@ class EnrolmentAPIClient(BaseAPIClient):
             self.endpoints['enrolment'],
             data=form_data
         )
+
+    def retrieve_trusted_source_signup_details(self, code):
+        url = self.endpoints['trusted-code'].format(code=code)
+        return self.get(url)
