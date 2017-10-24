73d3be0239aacbf78aa00bd6d3bc06d03fdb9d3e
richtier <rikatee@gmail.com>
2017-10-24 11:11:58 +0100

    Allow trige result to be updated

--- directory_api_client/exportreadiness.py
+++ directory_api_client/exportreadiness.py
@@ -4,20 +4,27 @@ from directory_api_client.base import BaseAPIClient
 class ExportReadinessAPIClient(BaseAPIClient):
 
     endpoints = {
-        'create-retrieve-triage-result': 'export-readiness/triage/',
+        'triage-result': 'export-readiness/triage/',
         'create-retrieve-article-read': 'export-readiness/article-read/',
         'create-retrieve-task-completed': 'export-readiness/task-completed/'
     }
 
     def retrieve_triage_result(self, sso_session_id):
         return self.get(
-            self.endpoints['create-retrieve-triage-result'],
+            self.endpoints['triage-result'],
             sso_session_id=sso_session_id
         )
 
     def create_triage_result(self, form_data, sso_session_id):
         return self.post(
-            self.endpoints['create-retrieve-triage-result'],
+            self.endpoints['triage-result'],
+            data=form_data,
+            sso_session_id=sso_session_id
+        )
+
+    def update_triage_result(self, form_data, sso_session_id):
+        return self.patch(
+            self.endpoints['triage-result'],
             data=form_data,
             sso_session_id=sso_session_id
         )
