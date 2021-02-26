39a354e8b26506243fea241edc0bdf51af8612b5
Bob Meredith <bob.meredith@ntlworld.com>
2021-02-26 10:40:30 +0000

    Format

--- tests/test_dataservices.py
+++ tests/test_dataservices.py
@@ -60,11 +60,13 @@ def test_get_country_data(client, requests_mock):
 
     assert requests_mock.last_request.url == url
 
+
 def test_get_country_data_by_country(client, requests_mock):
     url = 'https://example.com/dataservices/country-data/'
     requests_mock.get(url)
-    client.get_country_data_by_country(countries=['FR','BE'], fields=['TestModel'])
+    client.get_country_data_by_country(countries=['FR', 'BE'], fields=['TestModel'])
     assert requests_mock.last_request.url == f'{url}?countries=FR&countries=BE&fields=TestModel'
+
 
 def test_get_cia_world_factbook_data(client, requests_mock):
     url = 'https://example.com/dataservices/cia-factbook-data/?country=China&data_key=government%2C+languages'
@@ -101,7 +103,7 @@ def test_get_society_data_by_country(client, requests_mock):
 def test_get_last_year_import_data_by_country(client, requests_mock):
     url = 'https://example.com/dataservices/lastyearimportdatabycountry/'
     requests_mock.get(url)
-    client.get_last_year_import_data_by_country(countries=['GE','BE'], commodity_code=123456)
+    client.get_last_year_import_data_by_country(countries=['GE', 'BE'], commodity_code=123456)
 
     assert requests_mock.last_request.url == f'{url}?commodity_code=123456&countries=GE&countries=BE'
 
